e085fc8c67bb1134ded90c48d9e33144f88ae3d6
nawhi <25394965+nawhi@users.noreply.github.com>
2019-01-23 13:02:02 +0000

    First test

---
+++ src/main/java/com/codurance/primefactors/PrimeFactors.java
@@ -0,0 +1,9 @@
+package com.codurance.primefactors;
+
+import java.util.Set;
+
+class PrimeFactors {
+    Set<Integer> factorise(int input) {
+        return Set.of(1);
+    }
+}
---
+++ src/test/java/com/codurance/primefactors/PrimeFactorsShould.java
@@ -0,0 +1,27 @@
+package com.codurance.primefactors;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PrimeFactorsShould {
+
+    @ParameterizedTest
+    @CsvSource(value={
+            "1:1"
+    }, delimiter=':')
+    void factorise_a_number_into_its_primes(int input, String rawOutput) {
+        Set<Integer> expectedOutput = Arrays.stream(rawOutput.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+
+        Set<Integer> actualOutput = new PrimeFactors().factorise(input);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
