ef5db43777a2b0917a5daff5b79d9b629fd00444
nawhi <25394965+nawhi@users.noreply.github.com>
2019-02-01 08:42:22 +0000

    Better name for method source

--- src/test/java/com/codurance/primefactors/PrimeFactorsShould.java
+++ src/test/java/com/codurance/primefactors/PrimeFactorsShould.java
@@ -1,31 +1,29 @@
 package com.codurance.primefactors;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PrimeFactorsShould {
 
     @ParameterizedTest
-    @CsvSource(value={
-            "1:1",
-            "2:2",
-            "3:3",
-            "4:2",
-            "5:5",
-    }, delimiter=':')
-    void factorise_a_number_into_its_primes(int input, String rawOutput) {
-        Set<Integer> expectedOutput = Arrays.stream(rawOutput.split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toSet());
-
+    @MethodSource("test_cases")
+    void factorise_a_number_into_its_primes(int input, Set<Integer> expectedOutput) {
         Set<Integer> actualOutput = new PrimeFactors().factorise(input);
 
         assertEquals(expectedOutput, actualOutput);
+    }
+
+    static Stream<Arguments> test_cases() {
+        return Stream.of(
+                Arguments.of(1, Set.of(1)),
+                Arguments.of(2, Set.of(2)),
+                Arguments.of(3, Set.of(3))
+        );
     }
 }
